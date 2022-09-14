07d217d2cfb616a2bcf0d65b6869892d2826c995
Jorge F Caria <jorgefcaria@gmail.com>
2022-09-14 10:29:03 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'app-ejemplo-busqueda-gps',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
