dec14be2a7260b9b7562a388ea63895ec75b1cd3
Jorge F Caria <jorgefcaria@gmail.com>
2022-09-15 08:47:45 +0200

    Buscador finalizado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+
+//IMPORT THE PLUGINS
+import { Geolocation } from '@ionic-native/geolocation/ngx';
+import { NativeGeocoder } from '@ionic-native/native-geocoder/ngx';
+
+
 @NgModule({
   declarations: [AppComponent],
+  entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
-  bootstrap: [AppComponent],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    //Añadir Geolocation y NativeGeocoder en los providers
+    Geolocation,    
+    NativeGeocoder,
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+  ],
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, NgZone } from '@angular/core';
+
+
+declare var google;
+
+const geocoder = new google.maps.Geocoder();
 
 @Component({
   selector: 'app-home',
@@ -6,7 +11,76 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  [x: string]: any;
+  
+  @ViewChild('map',  {static: false}) mapElement: ElementRef;
+  map: any;
+  address:string;
+  lat: string;
+  long: string;  
+  autocomplete: { input: string; };
+  autocompleteItems: any[];
+  location: any;
+  placeid: any;
+  GoogleAutocomplete: any;
+  coordenadas: any;
+  label_coordenadas:any;
 
-  constructor() {}
+  constructor(
+        
+    public zone: NgZone,
+  ) {
+    this.GoogleAutocomplete = new google.maps.places.AutocompleteService();
+    this.autocomplete = { input: '' };
+    this.autocompleteItems = [];
+  }
+ 
+  
+  //AUTOCOMPLETE, SIMPLEMENTE ACTUALIZAMOS LA LISTA CON CADA EVENTO DE ION CHANGE EN LA VISTA.
+  UpdateSearchResults(){
+    if (this.autocomplete.input == '') {
+      this.autocompleteItems = [];
+      return;
+    }
+    this.GoogleAutocomplete.getPlacePredictions({ input: this.autocomplete.input },
+    (predictions, status) => {
+      this.autocompleteItems = [];
+      this.zone.run(() => {
+        predictions.forEach((prediction) => {
+          this.autocompleteItems.push(prediction);
+        });
+      });
+    });
+  }
+  
+  //FUNCION QUE LLAMAMOS DESDE EL ITEM DE LA LISTA.
+  SelectSearchResult(item) {
+    //AQUI PONDREMOS LO QUE QUERAMOS QUE PASE CON EL PLACE ESCOGIDO, GUARDARLO, SUBIRLO A FIRESTORE.
+    //HE AÑADIDO UN ALERT PARA VER EL CONTENIDO QUE NOS OFRECE GOOGLE Y GUARDAMOS EL PLACEID PARA UTILIZARLO POSTERIORMENTE SI QUEREMOS.
+    
+    //alert(JSON.stringify(item))      
+    this.placeid = item.place_id
+
+    geocoder.geocode({ address: item.description }, (results, status) => {
+      if (status === "OK") {
+          // Resultados en consola
+          console.log(results);
+
+          this.coordenadas = results[0].geometry.location;
+          this.label_coordenadas = 'Coordenadas:'
+          
+      } else {
+          alert("Geocode error: " + status);
+      }
+  });
+  
+  } 
+  
+  //LLAMAMOS A ESTA FUNCION PARA LIMPIAR LA LISTA CUANDO PULSAMOS IONCLEAR.
+  ClearAutocomplete(){
+    this.autocompleteItems = []
+    this.autocomplete.input = ''
+  }
+ 
 
 }
